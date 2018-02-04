c624f50add106f2b2d9d9c8a6fdcd41b6b91fa26
Dillion Verma <dillionverma@hotmail.com>
2018-02-03 23:33:42 -0800

    enable pg database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180204005204) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cryptos", force: :cascade do |t|
     t.string "name"
     t.string "symbol"

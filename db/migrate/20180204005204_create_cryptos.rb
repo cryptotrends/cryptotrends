ac6c8e8ba6dcc9d06f1a730ed8c69beef063d041
marc-li <marc.liddd@gmail.com>
2018-02-04 09:59:33 -0800

    add url to seed (#13)
    * add url to seed
    
    * change url

--- db/migrate/20180204005204_create_cryptos.rb
+++ db/migrate/20180204005204_create_cryptos.rb
@@ -14,6 +14,7 @@ class CreateCryptos < ActiveRecord::Migration[5.1]
       t.decimal :tweets_sentiment
       t.decimal :reddit_sentiment
       t.integer :reddit_posts_24h
+      t.string :github_url
       t.decimal :github_index
 
       t.timestamps

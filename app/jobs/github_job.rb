75bb4bcfd961e1246067cad7db4375e42e4fa3e4
Marc Li <marc.liddd@gmail.com>
2018-02-04 08:44:49 -0800

    fix bug

--- app/jobs/github_job.rb
+++ app/jobs/github_job.rb
@@ -4,20 +4,19 @@ class GithubJob < ApplicationJob
 
   def perform
     Crypto.all.each do |crypto|
-      if !crypto[:github_id].nil?
-        binding.pry
-        get_github_for(crypto[:github_id], crypto[:symbol])
+      if !crypto.github_id.nil?
+        get_github_for(crypto)
       end
     end
   end
 
-  def get_github_for(link, symbol)
-    commit_array = OctoClient.commits(link)
+  def get_github_for(crypto)
+    commit_array = OctoClient.commits(crypto.github_id)
     x = 0
     y = 0
     commit_array.each do |commit|
       sha = commit["sha"]
-      stats = OctoClient.commit(link, sha)["stats"]
+      stats = OctoClient.commit(crypto.github_id, sha)["stats"]
       if stats.nil?
         y += 1
         next
@@ -25,15 +24,8 @@ class GithubJob < ApplicationJob
       temp = stats["deletions"] - stats["additions"]
       x += get_index(temp)
     end
-    binding.pry
     x /= (commit_array.size - y)
-    binding.pry
-    u = Crypto.find_by(symbol: symbol)
-    if !u
-      u = Crypto.create!(symbol: symbol)
-    end
-    binding.pry
-    u.update_attributes(github_index: x)
+    crypto.update_attributes(github_index: x)
   end
 
   def get_index(temp)
